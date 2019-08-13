d4a1de96db36b86805dd2874cfa8166600b0b58e
NapoleonDLP <napoleon.dlp@gmail.com>
2019-08-13 14:29:33 -0700

    renders test text from app.jsx

--- challenge_1/client/components/app.jsx
+++ challenge_1/client/components/app.jsx
@@ -1,2 +1,18 @@
+import React from 'react';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>TESTER</h1>
+      </div>
+    )
+  }
+}
+
+export default App
