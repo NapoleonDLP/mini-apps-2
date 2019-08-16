33521fbb02e1c26f9fc3395f5d298bbbfa7cb4ed
NapoleonDLP <napoleon.dlp@gmail.com>
2019-08-15 17:28:31 -0700

    builds Frames component and renders rows

---
+++ challenge_3/client/components/Frames.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Rows from './Rows.jsx';
+
+function Frames (props) {
+  return (
+    <div styles="display: flex;" id="frames">
+      <Rows />
+    </div>
+  )
+}
+
+export default Frames;
