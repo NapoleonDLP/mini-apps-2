a66dce9344af82140f873f839dd7bfdde185f00a
NapoleonDLP <napoleon.dlp@gmail.com>
2019-08-15 17:24:35 -0700

    builds app component and renders all Frames

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Frames from './components/Frames.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,7 +11,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <h1>Hello World!</h1>
+        <Frames />
       </div>
     )
   }
