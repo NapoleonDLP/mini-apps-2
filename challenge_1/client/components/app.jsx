c465b722fb5fb0b14349b76fc4c703fb1e10b7ec
NapoleonDLP <napoleon.dlp@gmail.com>
2019-08-16 11:21:48 -0700

    adjusts spacing

--- challenge_1/client/components/app.jsx
+++ challenge_1/client/components/app.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import Events from './Events.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
+    
   }
 
   render() {
     return (
-      <div>
-        <h1>TESTER</h1>
+      <div id="app">
+        <h1>Historical Events Finder</h1>
+        <Events />
       </div>
     )
   }
