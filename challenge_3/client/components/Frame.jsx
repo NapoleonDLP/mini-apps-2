9cfedf96c3bc6e018a22520fac6526028cac07de
NapoleonDLP <napoleon.dlp@gmail.com>
2019-08-16 10:53:19 -0700

    remove styling attribute

--- challenge_3/client/components/Frame.jsx
+++ challenge_3/client/components/Frame.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Frame ({ pin }) {
 
   if(pin === 'I') {
-    return <div syles="width:100px;" id="frame">I</div>
+    return <div id="frame">I</div>
   } else if (pin === null) {
     return <div id="noPin">.</div>
   }
